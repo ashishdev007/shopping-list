6e4eaea49c603daba53ba83495b1e0de3b3a6753
Ashish Dev <ashishdevofficial@outlook.com>
2019-12-26 08:09:47 -0600

    Made a shopping list item model and added route to get all items

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const items = require("./routes/api/items");
+
 app.use(bodyParser.json());
 
 //DB Config
@@ -13,6 +15,9 @@ mongoose
     .connect(db)
     .then(() => console.log("Connected to database!"))
     .catch(err => console.log(err));
+
+//Routes
+app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 
