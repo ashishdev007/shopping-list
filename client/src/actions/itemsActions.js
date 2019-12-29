023659165888139da8770730320e2d87ad447ecf
Ashish Dev <ashishdevofficial@outlook.com>
2019-12-29 16:12:29 -0600

    Add and delete connection established

--- client/src/actions/itemsActions.js
+++ client/src/actions/itemsActions.js
@@ -1,28 +1,60 @@
 import { FETCH_ITEMS, ADD_ITEM, DELETE_ITEM } from "./types";
 import axios from "../apis/axios";
+import { returnErrors } from "./errorActions";
 
 export const fetchItems = () => async dispatch => {
-    const res = await axios.get("/api/items");
+    try {
+        const res = await axios.get("/api/items");
 
-    dispatch({
-        type: FETCH_ITEMS,
-        payload: res.data
-    });
+        dispatch({
+            type: FETCH_ITEMS,
+            payload: res.data
+        });
+    } catch (err) {
+        const { data, status } = err.response;
+        dispatch(returnErrors(data.msg, status, FETCH_ITEMS + "_FAIL"));
+    }
 };
 
-export const addItem = name => async dispatch => {
-    const res = await axios.post("/api/items", { name: name });
+export const addItem = name => async (dispatch, getState) => {
+    try {
+        const res = await axios.post(
+            "/api/items",
+            { name: name },
+            {
+                headers: {
+                    "Content-type": "application/json",
+                    "x-auth-token": getState().auth.token
+                }
+            }
+        );
 
-    dispatch({
-        type: ADD_ITEM,
-        payload: res.data
-    });
+        console.log(res);
+        dispatch({
+            type: ADD_ITEM,
+            payload: res.data
+        });
+    } catch (err) {
+        const { data, status } = err.response;
+        dispatch(returnErrors(data.msg, status, ADD_ITEM + "_FAIL"));
+    }
 };
 
-export const deleteItem = id => async dispatch => {
-    await axios.delete(`/api/items/${id}`);
-    dispatch({
-        type: DELETE_ITEM,
-        payload: id
-    });
+export const deleteItem = id => async (dispatch, getState) => {
+    try {
+        await axios.delete(`/api/items/${id}`, {
+            headers: {
+                "Content-type": "application/json",
+                "x-auth-token": getState().auth.token
+            }
+        });
+        dispatch({
+            type: DELETE_ITEM,
+            payload: id
+        });
+    } catch (err) {
+        const { data, status } = err.response;
+
+        dispatch(returnErrors(data.msg, status, DELETE_ITEM + "_FAIL"));
+    }
 };
