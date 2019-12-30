054de07742b0ead601f1bff2da74a470888a18ca
Ashish Dev <ashishdevofficial@outlook.com>
2019-12-30 14:08:46 -0600

    deploy ready

--- server.js
+++ server.js
@@ -3,28 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const config = require("config");
 const cors = require("cors");
+const path = require("path");
 
 app.use(express.json());
 
 app.use(cors());
-
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     if (req.method === "OPTIONS") {
-//         res.header(
-//             "Access-Control-Allow-Methods",
-//             "GET, PUT, POST, PATCH, DELETE"
-//         );
-//         return res.status(200).json({});
-//     }
-
-//     next();
-// });
 
 //DB Config
 const db = config.get("mongoURI");
@@ -43,6 +26,15 @@ mongoose
 app.use("/api/items", require("./routes/api/items"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+//static assets in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const port = process.env.PORT || 1500;
 
